7bdfa4830b3b0f0668f78b9dfc90a9a5b82fbd75
João Conrad <joao.conrad@opens.com.br>
2022-10-18 20:10:02 -0300

    Fix _blank

--- pages/index.js
+++ pages/index.js
@@ -342,7 +342,7 @@ export default function Home() {
         <a
           className="font-base max-w-fit"
           href="https://github.com/Conrage"
-          target="_blank"
+          target="blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
