c3a8eaf6d6c56b82a1c9d9c0d1d3d17721e7accb
João Conrad <joao.conrad@opens.com.br>
2022-10-18 12:26:54 -0300

    New design, FPL NA and EU

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <title>FPL Live Matches</title>
+                <meta name="description" content="Website to watch FPL live matches and twitch streams" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossorigin
+                ></link>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap"
+                    rel="stylesheet"
+                ></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
