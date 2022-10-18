9ee5f7d0dffca23d520f3b979bee6152f4fb897e
João Conrad <joao.conrad@opens.com.br>
2022-10-18 20:02:27 -0300

    Add players on queue and map

--- pages/api/queue/[id].js
+++ pages/api/queue/[id].js
@@ -2,10 +2,11 @@
 import axios from "axios";
 export default function handler(req, res) {
   const { id } = req.query;
+  const config = {
+    headers: { Authorization: `Bearer ${process.env.TOKEN_FACEIT}` },
+  };
   axios
-    .get(
-      `https://api.faceit.com/stream/v2/streamings?competition_id=${id}&competition_type=hub&limit=10&offset=0`
-    )
+    .get(`https://api.faceit.com/queue/v1/player/${id}?limit=15`, config)
     .then((response) => {
       res.status(200).json(response.data || []);
     });
