4528bd1528fdd1d541edf268e27151b9ea91f437
João Conrad <joao.conrad@opens.com.br>
2022-10-19 09:39:00 -0300

    Handle unsuccessful requests

--- pages/api/matches/[id].js
+++ pages/api/matches/[id].js
@@ -1,12 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios";
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { id } = req.query;
-  axios
-    .get(
+  try {
+    const response = await axios.get(
       `https://api.faceit.com/match/v2/match?entityType=hub&entityId=${id}&state=SUBSTITUTION,CAPTAIN_PICK,VOTING,CONFIGURING,READY,ONGOING,MANUAL_RESULT,PAUSED,ABORTED&limit=20&offset=0`
-    )
-    .then((response) => {
-      res.status(200).json(response.data || []);
-    });
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ error: e.message });
+  }
 }
