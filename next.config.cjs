73d19bff59910c34c40e8669e8ca73b879791cce
João Conrad <joao.conrad@opens.com.br>
2023-03-27 18:06:39 -0300

    fix: remove eslint build

--- next.config.cjs
+++ next.config.cjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
